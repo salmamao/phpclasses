90a475fcd36b3252aab14ecb68a7bd6d2f154c56
Younes Khoubza <younes.khoubza@gmail.com>
2016-08-22 17:42:05 +0100

    redirection class

---
+++ app/Classes/Redirect.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Classes;
+
+class Redirect {
+    public static function to($location = null) {
+        if ($location) {
+            if (is_numeric($location)) {
+                switch ($location) {
+                    case 404:
+                        header('HTTP/1.0 404 Not Found');
+                        exit();
+                        break;
+                    case 301 :
+                        header('HTTP/1.1 301 Moved Permanently');
+                        exit();
+                        break;
+                }
+            }
+            header('Location:' . $location);
+            exit();
+        }
+    }
+}

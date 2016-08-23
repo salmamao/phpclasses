6dc898eb836c802ef1ec2d0358ebf38bb7a9e7b9
Younes Khoubza <younes.khoubza@gmail.com>
2016-08-23 07:05:02 +0100

    add configuration to init file

--- app/Classes/Config.php
+++ app/Classes/Config.php
@@ -5,9 +5,25 @@ namespace App\Classes;
 
 
 class Config {
+
     public static function get($path = null) {
         if ($path) {
-            $config = $GLOBALS[ 'config' ];
+
+            switch ($_SERVER[ 'SERVER_NAME' ]):
+                case 'local':
+                    $config = $GLOBALS[ 'config' ][ 'env' ][ 'local' ];
+                    break;
+
+                case 'website.com':
+                    $config = $GLOBALS[ 'config' ][ 'env' ][ 'production' ];
+                    break;
+
+                default:
+                    $config = null;
+                    break;
+            endswitch;
+
+
             $path = explode('/', $path);
 
             foreach ($path as $bit) {
--- core/init.php
+++ core/init.php
@@ -1,4 +1,16 @@
 <?php
-include_once __DIR__.'/../vendor/autoload.php';
+include_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Classes\DataBase;
+use App\Classes\Config;
+use App\Classes\Session;
 
 Session::start();
+
+$GLOBALS[ 'config' ] = include __DIR__ . '/../config/app.php';
+
+
+$bdd = new DataBase('mysql:host=' . Config::get('database/host') . ';dbname=' . Config::get('database/database') . ';charset=' . Config::get('database/charset'), Config::get('database/username'), Config::get('database/password'));
+function getError($error) { echo $error; }
+
+$bdd->setErrorCallbackFunction('getError');
